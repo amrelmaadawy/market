3800abeb36d350d3db8a7200a565b7321afb9fd0
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-02-02 18:32:15 +0200

    search text form field

--- lib/views/home/UI/home_view.dart
+++ lib/views/home/UI/home_view.dart
@@ -1,12 +1,34 @@
+import 'package:app/core/app_colors.dart';
+import 'package:app/views/auth/UI/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child:  Text('Home View'),
+    TextEditingController searchController = TextEditingController();
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          CustomTextFormFeild(
+              suffixIcon: Container(
+                decoration: BoxDecoration(
+                    color: AppColors.kPrimaryColor,
+                    borderRadius: BorderRadius.circular(8)),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search),
+                  color: AppColors.kWhiteColor,
+                ),
+              ),
+              lableText: 'Search in market',
+              controller: searchController,
+              obscureText: false,
+              keyboardType: TextInputType.text),
+        ],
+      ),
     );
   }
 }
