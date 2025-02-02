9c411c217f0762ad1558bdf837beaa55e6f06d82
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-02-02 20:40:14 +0200

    add categories list

--- lib/views/home/UI/home_view.dart
+++ lib/views/home/UI/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:app/core/app_colors.dart';
 import 'package:app/views/home/UI/widgets/search_form_feild.dart';
 import 'package:flutter/material.dart';
 
@@ -14,8 +15,58 @@ class HomeView extends StatelessWidget {
           SearchFormField(searchController: searchController),
           const SizedBox(height: 16),
           Image.asset('assets/images/buy.jpg'),
+          const SizedBox(height: 16),
+          const Text(
+            'Popular Categories',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: AppColors.kBlackColor,
+            ),
+          ),
+          const SizedBox(height: 16),
+          SizedBox(
+            height: 100,
+            child: ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: AppColors.kPrimaryColor,
+                        foregroundColor: AppColors.kWhiteColor,
+                        child: Icon(categorie[index].icon,size: 38,),
+                      ),
+                    ),
+                    Text(categorie[index].name),
+                  ],
+                );
+              },
+              itemCount: categorie.length,
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+            ),
+          ),
         ],
       ),
     );
   }
+}
+
+List<Categories> categorie = [
+  Categories(name: 'Sports', icon: Icons.sports),
+  Categories(name: 'Electronics', icon: Icons.tv),
+  Categories(name: 'Collections', icon: Icons.collections),
+  Categories(name: 'Books', icon: Icons.book),
+  Categories(name: 'Games', icon: Icons.games),
+  Categories(name: 'Bikes', icon: Icons.bike_scooter),
+];
+
+class Categories {
+  final String name;
+  final IconData icon;
+  Categories({required this.name, required this.icon});
 }
