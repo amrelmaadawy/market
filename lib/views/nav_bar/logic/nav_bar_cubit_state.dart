67ca43f87a6e6533e0da771ebc761a6e35957e75
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-02-02 18:21:30 +0200

    on change nav bar

---
+++ lib/views/nav_bar/logic/nav_bar_cubit_state.dart
@@ -0,0 +1,8 @@
+part of 'nav_bar_cubit_cubit.dart';
+
+
+abstract class NavBarCubitState {}
+
+class NavBarCubitInitial extends NavBarCubitState {}
+
+class NavBarChangeView extends NavBarCubitState {}
