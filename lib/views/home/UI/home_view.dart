856d05d1d1f6024d7ebe57f3d743fdf873ea067e
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-02-02 20:43:48 +0200

    refatoring categories

--- lib/views/home/UI/home_view.dart
+++ lib/views/home/UI/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:app/core/app_colors.dart';
+import 'package:app/views/home/UI/widgets/categories_list.dart';
 import 'package:app/views/home/UI/widgets/search_form_feild.dart';
 import 'package:flutter/material.dart';
 
@@ -25,48 +26,9 @@ class HomeView extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 16),
-          SizedBox(
-            height: 100,
-            child: ListView.separated(
-              separatorBuilder: (context, index) => const SizedBox(width: 20),
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: AppColors.kPrimaryColor,
-                        foregroundColor: AppColors.kWhiteColor,
-                        child: Icon(categorie[index].icon,size: 38,),
-                      ),
-                    ),
-                    Text(categorie[index].name),
-                  ],
-                );
-              },
-              itemCount: categorie.length,
-              scrollDirection: Axis.horizontal,
-              physics: const BouncingScrollPhysics(),
-            ),
-          ),
+          const CategoriesList(),
         ],
       ),
     );
   }
-}
-
-List<Categories> categorie = [
-  Categories(name: 'Sports', icon: Icons.sports),
-  Categories(name: 'Electronics', icon: Icons.tv),
-  Categories(name: 'Collections', icon: Icons.collections),
-  Categories(name: 'Books', icon: Icons.book),
-  Categories(name: 'Games', icon: Icons.games),
-  Categories(name: 'Bikes', icon: Icons.bike_scooter),
-];
-
-class Categories {
-  final String name;
-  final IconData icon;
-  Categories({required this.name, required this.icon});
 }
