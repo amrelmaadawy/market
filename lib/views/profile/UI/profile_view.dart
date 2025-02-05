8f97bdaae14442a125663af97ef742d575d3942b
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-02-05 08:43:40 +0200

    finish profile view

--- lib/views/profile/UI/profile_view.dart
+++ lib/views/profile/UI/profile_view.dart
@@ -1,3 +1,5 @@
+import 'package:app/core/app_colors.dart';
+import 'package:app/views/profile/UI/widgets/custom_profile_button.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
@@ -5,8 +7,57 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child:  Text('profile View'),
+    return SafeArea(
+      child: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            const CircleAvatar(
+              radius: 50,
+              backgroundColor: AppColors.kPrimaryColor,
+              child: Icon(
+                Icons.person,
+                color: AppColors.kWhiteColor,
+                size: 50,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Amr Elmaadawy',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              'amrelmaadawy@gmail.com',
+              style: TextStyle(fontSize: 15),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomProfileButton(
+              onPressed: () {},
+              icon: Icons.person,
+              text: 'Edit Name',
+            ),
+            CustomProfileButton(
+              onPressed: () {},
+              icon: Icons.badge,
+              text: 'My Orders',
+            ),
+            CustomProfileButton(
+              onPressed: () {},
+              icon: Icons.logout,
+              text: 'Log Out',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
