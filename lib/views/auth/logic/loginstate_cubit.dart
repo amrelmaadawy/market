8edb5bea00d7d75cc165a4ebab5513e87b177164
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-02-06 04:04:12 +0200

    login supabase p1

---
+++ lib/views/auth/logic/loginstate_cubit.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'loginstate_state.dart';
+
+class LoginstateCubit extends Cubit<LoginstateState> {
+  LoginstateCubit() : super(LoginstateInitial());
+
+  SupabaseClient client = Supabase.instance.client;
+
+  Future<void> login({required String email, required String password}) async {
+    emit(LoginstateLoading());
+    try {
+      await client.auth.signInWithPassword(password: password, email: email);
+      emit(LoginstateSuccesses());
+    } on AuthApiException catch (e) {
+      emit(LoginstateErorr(e.message));
+      if (kDebugMode) {
+        print(e.toString());
+      }
+    } catch (e) {
+      emit(LoginstateErorr(e.toString()));
+      if (kDebugMode) {
+        print(e.toString());
+      }
+    }
+  }
+}
---
+++ lib/views/auth/logic/loginstate_state.dart
@@ -0,0 +1,22 @@
+part of 'loginstate_cubit.dart';
+
+abstract class LoginstateState {}
+
+class LoginstateInitial extends LoginstateState {}
+
+class LoginstateLoading extends LoginstateState {}
+class LoginstateSuccesses extends LoginstateState {}
+class LoginstateErorr extends LoginstateState {
+  final String message;
+  LoginstateErorr(this.message);
+}
+
+
+class SignUpstateLoading extends LoginstateState {}
+class SignUpstateSuccesses extends LoginstateState {}
+class SignUpstateErorr extends LoginstateState {
+  final String message;
+  SignUpstateErorr(this.message);
+}
+
+
