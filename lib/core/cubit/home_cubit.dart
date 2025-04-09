a388c6acf3956893f0ac9bd8d05a34d4954d1dbc
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-04-09 03:10:39 +0200

    search logic p1

--- lib/core/cubit/home_cubit.dart
+++ lib/core/cubit/home_cubit.dart
@@ -2,13 +2,15 @@ import 'package:app/core/dio_servises.dart';
 import 'package:app/core/models/product_model/product_model.dart';
 import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
+
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
   final DioServises _dioServises = DioServises();
   List<ProductModel> products = [];
-  Future<void> getProduct() async {
+  List<ProductModel> searchResult = [];
+  Future<void> getProduct({String? quary}) async {
     emit(GetDataLoadingState());
     try {
       Response response = await _dioServises
@@ -16,10 +18,20 @@ class HomeCubit extends Cubit<HomeState> {
       for (var product in response.data) {
         products.add(ProductModel.fromJson(product));
       }
-
+      searchProduct(quary);
       emit(GetDataSuccesseState());
     } catch (e) {
       emit(GetDataErrorState(e.toString()));
+    }
+  }
+
+  void searchProduct(String? quary) {
+    if (quary != null) {
+      for (var product in products) {
+        if (product.productName!.toLowerCase().contains(quary.toLowerCase())) {
+          searchResult.add(product);
+        }
+      }
     }
   }
 }
